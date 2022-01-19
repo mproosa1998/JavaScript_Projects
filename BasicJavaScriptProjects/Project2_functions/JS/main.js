bfe7e6c56170ee43f823ee77df3a32ba499a4b48
mroosa1998 <56371790+mroosa1998@users.noreply.github.com>
2022-01-19 10:33:24 -0500

    myFIrstFunction assignment

---
+++ BasicJavaScriptProjects/Project2_functions/JS/main.js
@@ -0,0 +1,6 @@
+function My_First_Function(){  // Function declaration
+    var str = "This is the button text!"; // defining var str
+    str += " Concatinated String"; // concatinating str
+    document.getElementById("Button_Text").innerHTML = str; // changing button text to str
+    document.getElementById("test").innerHTML="SUCCESS!"; // changing paragrah text
+}
