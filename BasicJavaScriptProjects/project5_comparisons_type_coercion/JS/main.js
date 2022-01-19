16280cefd54bbd2e522cb78f1116386746880c20
mroosa1998 <56371790+mroosa1998@users.noreply.github.com>
2022-01-19 12:21:49 -0500

    coercion assignment

---
+++ BasicJavaScriptProjects/project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,22 @@
+document.writeln(typeof "waffle"); // found type of "waffle"
+document.writeln(0/0); // divided 0 by itself to produce NaN
+document.writeln(isNaN("string")); // used isNAN to see if 'string' was a type number
+document.writeln(isNaN(4)); // used isNAN to see if 4 was a type number
+document.writeln(2E310); // used and exponentially large number to write infinity
+document.writeln(-2E310) // used an exponentially low number to write -infinity
+document.writeln(3>4); // used > to display true
+document.writeln(3<4) // used < to display false
+console.log(2+2) // wrote sum of 2 numbers to console log
+console.log(3>4) // wrote false to console log
+document.writeln(3==3) // use == to display true
+document.writeln(3==4) // use == to display false
+document.writeln(3===3); // use === to display true
+document.writeln(3==='3') // use === to display false
+document.writeln(3===4); // use === to display false
+document.writeln(3==='4') // use === to display false
+document.writeln(3==3 && 3!=3); // use and to display false
+document.writeln(3==3 && 4==4) // use and to display true
+document.writeln(3==3 || 3==4); // use or to display true
+document.writeln(3==4 || 3>4) // use or to display false
+document.writeln(!(3==3)) // use ! to display false
+document.writeln(!(3!=3))  // use ! to display true
