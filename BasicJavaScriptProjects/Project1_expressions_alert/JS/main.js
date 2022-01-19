ab7efbce2fe624dd8e70bbe28e57f34eb3b9b440
mroosa1998 <56371790+mroosa1998@users.noreply.github.com>
2022-01-19 11:15:55 -0500

    Update main.js

--- BasicJavaScriptProjects/Project1_expressions_alert/JS/main.js
+++ BasicJavaScriptProjects/Project1_expressions_alert/JS/main.js
@@ -3,7 +3,8 @@ window.alert("Hello, World! 2.0") //alert box hello world
 document.write("Hello, World! 3.0") //print to screen hello world
 
 var a ="Hello,World! 4.0", b ="Hello, World! 5.0",  //assigning to multiple variables
-    c = "Hello, \"World\"!", d ="Hello" + " World"
+    c = "Hello, \"World\"!", d ="Hello" + " World",
+    con="concat", cat="ination"
 
 document.write(a.fontcolor("blue")) //var a out
 
@@ -14,3 +15,5 @@ document.write(c) // var c out
 document.write(d) //var d out
 
 document.write(3+3) // expression out
+
+document.write(con+cat) //concatination
