a509d34b066c75e8046bd6fb174df15309725477
mroosa1998 <56371790+mroosa1998@users.noreply.github.com>
2022-01-19 10:13:17 -0500

    JS alert assignment

---
+++ BasicJavaScriptProjects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,16 @@
+alert("Hello World!") //alert box hello world
+window.alert("Hello, World! 2.0") //alert box hello world
+document.write("Hello, World! 3.0") //print to screen hello world
+
+var a ="Hello,World! 4.0", b ="Hello, World! 5.0",  //assigning to multiple variables
+    c = "Hello, \"World\"!", d ="Hello" + " World"
+
+document.write(a.fontcolor("blue")) //var a out
+
+alert(b) //var b out
+
+document.write(c) // var c out
+
+document.write(d) //var d out
+
+document.write(3+3) // expression out
